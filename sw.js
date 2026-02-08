8e803a2f6315dfae877ebd54238cbbc8a3e9a6a1
Pakyutu <digipossaya@gmail.com>
2026-02-09 00:41:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sales-pro-v10'; // Naikkan angkanya (v2 -> v3 -> v4 dst)
+const CACHE_NAME = 'sales-pro-v11'; // Naikkan angkanya (v2 -> v3 -> v4 dst)
 const urlsToCache = [
   './',
   './index.html',
@@ -44,6 +44,7 @@ self.addEventListener('activate', event => {
   );
   self.clients.claim(); // Langsung ambil alih kontrol
 });
+
 
 
 
